24918ca5f71955d12fbc88c14397dc1750d33ce7
DieissonNarde <dieissonnarde@gmail.com>
2022-05-07 17:50:03 -0400

    aula 03

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "10c05c020112f3",
+    pass: "032bf95a61dc28"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Dieisson Narde <dieissonnarde@gmail.com',
+      subject: subject,
+      html: body,
+    })
+  };
+}
